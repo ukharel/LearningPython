59759edb52667b48d4475190ce35520a4de86818
Ujjwal <phoenixgene9271@gmail.com>
2025-12-19 18:02:58 +0545

    Typecasting

---
+++ 02_typecasting.py
@@ -0,0 +1,12 @@
+# Typecasting  = it is the process of converting a variables into different data type ( str(), int(), float(), bool())
+
+food = "Pizza"
+quantity = 5
+price = 3.99
+is_hungry = True
+
+
+print(str(is_hungry))
+print(bool(food))
+print(int(price))
+print(float(quantity))
