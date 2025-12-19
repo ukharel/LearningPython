4404887486cf28ff64123ba144879927fee68449
Ujjwal <phoenixgene9271@gmail.com>
2025-12-19 19:14:46 +0545

    If else

---
+++ ifelse.py
@@ -0,0 +1,16 @@
+# If/Else : It runs the block of code when the condition is True else run something else
+
+'''score = int(input('Enter your score in MBA? '))
+
+if score > 90:
+    print("Let's goto the Bali. ")
+else:
+    print("Let's study hard for next year. ")
+'''
+
+name = input('Enter your name? ')
+
+if name == '':
+    print('Please enter your name. ')
+else:
+    print(f'Your name is {name}')
