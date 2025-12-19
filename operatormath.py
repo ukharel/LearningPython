cd60a120878c27f9f14a33f1e97cb2284ef0a5bd
Ujjwal <phoenixgene9271@gmail.com>
2025-12-19 18:59:25 +0545

    Operators and math

---
+++ operatormath.py
@@ -0,0 +1,29 @@
+# operator performs some operations in the expression 
+
+'''chocolate = 2
+
+chocolate += 2
+print(chocolate)
+
+chocolate -=2
+chocolate *= 3
+chocolate /= 2
+chocolate **= 3
+chocolate %=2
+
+print(chocolate)
+'''
+
+import math
+
+x= 3.2
+y = -4
+z= 9.9434
+
+print(round(z,2))
+print(abs(y))
+print(pow(x,3))
+
+print(math.pi)
+print(math.ceil(z))
+print(math.floor(x))
