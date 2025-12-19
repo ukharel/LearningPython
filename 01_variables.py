6f64465aad0b2bb366b49ef4334eb48521ab8261
Ujjwal <phoenixgene9271@gmail.com>
2025-12-19 17:53:15 +0545

    variables and datatypes

---
+++ 01_variables.py
@@ -0,0 +1,27 @@
+# Variables are the containers or box to store value. Value may range anything from string, integer, float or boolean.
+
+# String= Value inside the this -> ' ' or " "
+name= "Ujjwal Kharel"
+Hobby = "Thinking"
+Want = "Present"
+
+# Integer - Nunberic value
+num1= 32
+age= 23
+amount= 0
+
+# Float - decimal value
+pi= 3.14
+distance = 43.2
+score = 6.5
+
+# Boolean - True or False
+
+You = True
+Me = False
+
+
+if You:
+    print('You are great. ')
+else:
+    print(' You are going to be great. ')
