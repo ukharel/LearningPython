fe87042ff72288fcd2604987157cce309a0ce1a1
Ujjwal <phoenixgene9271@gmail.com>
2025-12-21 09:14:24 +0545

    Indexing in string

---
+++ 11_indexing.py
@@ -0,0 +1,13 @@
+# Indexing - accessing the element from the sequences using []
+
+number = "9828-2492-2428-902"
+
+print(number[0])
+print(number[:4])
+print(number[3:])
+print(number[1:5])
+print(number[-1])
+print(number[-5:])
+print(number[::-1])
+print(number[::3])
+
