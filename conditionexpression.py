01ae169294cb4abc9330df3a6a97a49f6089d58c
Ujjwal <phoenixgene9271@gmail.com>
2025-12-21 08:50:10 +0545

    conditional expression

---
+++ conditionexpression.py
@@ -0,0 +1,10 @@
+# condition expression- one shortcut for if-else statement.
+
+temp = 20
+
+result = "Hot" if temp > 21 else "Cold"
+print(result)
+
+num = 6
+status = "Even" if num % 2 == 0 else "Odd"
+print(status)
