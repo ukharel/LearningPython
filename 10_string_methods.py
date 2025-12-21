b12afc4264fbc5b512403a82456a15eb0ba6d4e2
Ujjwal <phoenixgene9271@gmail.com>
2025-12-21 08:58:27 +0545

    string methods

---
+++ 10_string_methods.py
@@ -0,0 +1,14 @@
+# string method- some methods which helps to manipulate the strings.
+
+name = input('Enter your name : ')
+
+print(len(name))
+print(name.find('l'))
+print(name.rfind('l'))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count('l'))
+print(name.replace('U', 'A'))
