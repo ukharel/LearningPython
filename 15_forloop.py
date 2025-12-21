08bc502a5c110ce1cc4deaed34f13c0df65e1791
Ujjwal <phoenixgene9271@gmail.com>
2025-12-21 19:22:04 +0545

    For loop basis

---
+++ 15_forloop.py
@@ -0,0 +1,11 @@
+# for loop - run the block of code for fixed time
+# it can iterate over list, string, sequence or anything that are iterable.
+
+
+for i in range(10,0,-1):
+    print(i)
+
+total=0
+for x in range(0,101):
+    total = total + x
+print(total)
