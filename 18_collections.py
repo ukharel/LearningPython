e0b85e24930fea8fa53024cb85c74eeef4c84da6
Ujjwal <phoenixgene9271@gmail.com>
2025-12-21 20:40:44 +0545

    Collections and its methods

---
+++ 18_collections.py
@@ -0,0 +1,35 @@
+# collections are the set of items 
+# List are ordered and changeable and duplicates are okay to use
+# set are unordered and ummutable , add and removing can be done but duplicates are not okay
+# tuple are ordered and unchangeable but duplicates are okay
+
+
+# food = ["Pizza","Burger","Momos","Chowmein","Pasta"]
+
+# print(food.index("Burger"))
+# print(food[0])
+# print(food[2]=='Sushi')
+#food[3]="Sushi"
+# print(food.insert(4,"Sushi"))
+# print(dir(food))
+# print(len(food))
+# print(food.count("Pizza"))
+# print(food)
+# food.sort(reverse=True)
+# print(food)
+
+
+#for i in food:
+ #   print(i)
+
+
+# shoes= { 'Nike', "jordan","adidas","puma"}
+
+# shoes.add('Timberland')
+# shoes.remove("Nike")
+# print(shoes)
+
+cars = ('Ferrari',"Choverlet","Porsche","Tesla")
+print(dir(cars))
+
+print(cars[2])
